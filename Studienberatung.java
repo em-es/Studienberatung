3da5e2f3a88cb03df77cc292a9543c6d3ab765ee
Marjan Stössner <marjanstoessner@haw-hamburg.de>
2013-10-23 20:05:05 +0200

    added Bist du wissbegierig?

--- Studienberatung.java
+++ Studienberatung.java
@@ -5,5 +5,23 @@ public class Studienberatung {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Hast du Abitur?");
 		String answer = scanner.next();
+		if (answer.equalsIgnoreCase("nein")) {
+			System.out.println("Hast du Fachabitur?");
+			String answer2 = scanner.next();
+			if (answer2.equalsIgnoreCase("nein")) {
+				System.out.println("Nicht studieren");
+			} else {
+			}
+
+		} else {
+			System.out.println("Bist du wissbegierig?");
+			String answer3 = scanner.next();
+			if (answer3.equalsIgnoreCase("nein")) {
+				System.out.println("Nicht studieren");
+			} else {
+				System.out.println("Auf jeden Fall studieren");
+
+			}
+		}
 	}
 }
