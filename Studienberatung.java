72b3fb870008b3a18d4b1acdd38a0d211c12d51c
Marjan Stössner <marjanstoessner@haw-hamburg.de>
2013-10-23 19:41:49 +0200

    added Hast du Fachabitur?

--- Studienberatung.java
+++ Studienberatung.java
@@ -5,5 +5,14 @@ public class Studienberatung {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Hast du Abitur?");
 		String answer = scanner.next();
+		if (answer.equalsIgnoreCase("nein")) {
+			System.out.println("Hast du Fachabitur?");
+			String answer2 = scanner.next();
+			if (answer2.equalsIgnoreCase("nein")) {
+				System.out.println("Nicht studieren");
+			} else {
+			}
+		} else {
+		}
 	}
 }
